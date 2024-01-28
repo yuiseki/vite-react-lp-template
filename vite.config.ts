2f5f90db8fa782e593ce695f364de34b63c52c72
yuiseki <yuiseki@gmail.com>
2024-01-28 16:33:02 +0900

    use process.env.BASE in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // TODO: You need to change this to your repo name
-  base: "/vite-react-lp-template/",
-});
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  return defineConfig({
+    plugins: [react()],
+    base: process.env.BASE ? process.env.BASE : "/vite-react-lp-template/",
+  });
+};
